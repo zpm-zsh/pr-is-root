f998ce937ce569e5909f6ba188cad6cdbd2b4601
Grigorii Horos <horosgrisa@gmail.com>
2019-08-09 01:08:16 +0300

    Fix colors

--- pr-is-root.plugin.zsh
+++ pr-is-root.plugin.zsh
@@ -7,12 +7,7 @@ _pr_is_root() {
   pr_is_root=""
   
   if [[ "$EUID" == 0 ]]; then
-    
-    if [[ $CLICOLOR = 1 ]]; then
-      pr_is_root="$PR_IS_ROOT_PREFIX%{${c[red]}${c_bold}${c_dim}%}root%{$c_reset%}$PR_IS_ROOT_SUFFIX"
-    else
-      pr_is_root="$PR_IS_ROOT_PREFIX""root$PR_IS_ROOT_SUFFIX"
-    fi
+    pr_is_root="$PR_IS_ROOT_PREFIX%{${c[red]}${c_bold}${c_dim}%}root%{$c_reset%}$PR_IS_ROOT_SUFFIX"
   fi
 }
 
