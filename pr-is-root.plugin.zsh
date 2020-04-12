264f10dc43ee7f470b564fcc35e290f78d7924b5
Grigorii Horos <horosgrisa@gmail.com>
2020-04-12 22:10:44 +0300

    Optimize

--- pr-is-root.plugin.zsh
+++ pr-is-root.plugin.zsh
@@ -3,5 +3,5 @@
 : ${PR_IS_ROOT_SUFFIX:=''}
 
 if [[ "$EUID" == 0 ]]; then
-  typeset -g pr_is_root="$PR_IS_ROOT_PREFIX%{${c[red]}${c_bold}${c_dim}%}root%{$c_reset%}$PR_IS_ROOT_SUFFIX"
+  typeset -g pr_is_root="$PR_IS_ROOT_PREFIX%{${c[red]}${c[bold]}${c[dim]}%}root%{${c[reset]}%}$PR_IS_ROOT_SUFFIX"
 fi
