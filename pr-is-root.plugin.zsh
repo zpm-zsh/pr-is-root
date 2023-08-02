8668df3ecce37b8862b7ff100fbfa09fa3edd9dd
Grigorii Horos <horosgrisa@gmail.com>
2023-08-03 00:55:13 +0300

    Remove prefix/suffix

--- pr-is-root.plugin.zsh
+++ pr-is-root.plugin.zsh
@@ -1,7 +1,5 @@
 #!/usr/bin/env zsh
-: ${PR_IS_ROOT_PREFIX:=' '}
-: ${PR_IS_ROOT_SUFFIX:=''}
 
 if [[ "$EUID" == 0 ]]; then
-  typeset -g pr_is_root="$PR_IS_ROOT_PREFIX%{${c[red]}${c[bold]}${c[dim]}%}root%{${c[reset]}%}$PR_IS_ROOT_SUFFIX"
+  typeset -g pr_is_root=" %{${c[red]}${c[bold]}${c[dim]}%}root%{${c[reset]}%}"
 fi
